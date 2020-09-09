0dddd062f3e3249367e29ce1c539886ee05c4d4a
gaganbaheti2000 <gaganbaheti.it@gmail.com>
2020-09-08 23:06:01 -0700

    hjfhfhfhj

--- pages/connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require_once "config.php";
-
-	$conn = new mysqli($servername,$username,$password,$database);
-
-	if ($conn->connect_error) {
-		die("connection failed:" . $conn->connect_error);
-	}
-?>
